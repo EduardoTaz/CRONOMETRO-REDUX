0a8e8c3eba830ba49207d87df7ecd501acecb1d3
Eduardo tazoi <edutazoi21@hotmail.com>
2025-02-28 12:22:02 -0300

    fix

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux'; 
+import store from './redux/store'; 
 import ScreenA from './screens/ScreenA';
 import ScreenB from './screens/ScreenB';
 
@@ -8,20 +10,14 @@ const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer
-      children={
-        <Stack.Navigator initialRouteName="ScreenA">
-          <Stack.Screen
-            name="ScreenA"
-            component={ScreenA}
-          />
-          <Stack.Screen
-            name="ScreenB"
-            component={ScreenB}
-          />
+    <Provider store={store}> 
+      <NavigationContainer>
+        <Stack.Navigator id={undefined} initialRouteName="ScreenA">
+          <Stack.Screen name="ScreenA" component={ScreenA} />
+          <Stack.Screen name="ScreenB" component={ScreenB} />
         </Stack.Navigator>
-      }
-    />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
